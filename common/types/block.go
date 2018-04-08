6c5e2f060141a2606fbda801dc5f59b73ed67a33
Gong Zibin <gzb1985@gmail.com>
2018-04-08 11:13:12 +0800

    block struct definition

--- common/types/block.go
+++ common/types/block.go
@@ -2,38 +2,97 @@ package types
 
 import (
 	//"math/big"
+	"fmt"
+	"bytes"
+	"io"
+	"crypto/sha256"
+
+	"github.com/bottos-project/core/library"
 )
 
 type Block struct {
-	header			Header
-	trxs		 	[]Transaction
+	header			*Header
+	transactions	[]*Transaction
 }
 
 type Header struct {
-	PrevBlockHash	[]byte
-	Number      	int
+	PrevBlockHash	library.Hash		// Hash of Previos block
+	Number      	uint32				// Block Number
+	Timestamp       uint32         		// Creation time
+	MerkleRoot		library.Hash
+	Producer		library.AccountName
+	ProducerChange	[]library.AccountName
+	ProducerSign	library.Hash	// TODO ECSDA sign type
 }
 
-func NewHeader(prevHash []byte, number int) *Header {
-	h := Header{PrevBlockHash: prevHash, Number: number}
-	return &h
-}
+func NewBlock(h *Header, txs []*Transaction) *Block {
+	b := Block{header: copyHeader(h)}
 
-func NewBlock(h *Header) *Block {
-	b := Block{header: *h, trxs: []Transaction{}}
+	if len(txs) == 0 {
+	} else {
+		// TODO Compute Hash
+		b.transactions = make([]*Transaction, len(txs))
+		copy(b.transactions, txs)
+	}
+
 	return &b
 }
 
-func (b *Block) Decode() error {
+func (h *Header) Serialize(w io.Writer) error {
+
 	return nil
 }
 
-func (b *Block) Encode() error {
+func (h *Header) Deserialize(r io.Reader) error {
+
+	return err
+}
+
+func (b *Block) Serialize(w io.Writer) error {
+	b.header.Serialize(w)
+
 	return nil
 }
 
-func (b *Block) Hash() []byte {
-	return []byte{}
+
+func (b *Block) Deserialize(r io.Reader) error {
+	var header Header
+	err := header.Deserialize(r)
+	if err != nil {
+		return fmt.Errorf("Header Deserialize failed: %s", err)
+	}
+
+	// TODO
+
+	b.header = &header
+	return nil
 }
 
-func (b *Block) Number() int     { return b.header.Number }
+func (h *Header) Hash() library.Hash {
+	value := bytes.NewBuffer(nil)
+	h.Serialize(value)
+	temp := sha256.Sum256(value.Bytes())
+	return temp
+}
+
+func (b *Block) Hash() library.Hash {
+	return b.header.Hash()
+}
+
+func copyHeader(h *Header) *Header {
+	cpy := *h
+
+	// TODO
+
+	return &cpy
+}
+
+func (b *Block) PrevBlockHash() library.Hash		{ return b.header.PrevBlockHash }
+func (b *Block) Number() uint32     				{ return b.header.Number }
+func (b *Block) Time() uint32						{ return b.header.Timestamp }
+func (b *Block) MerkleRoot() library.Hash			{ return b.header.MerkleRoot }
+func (b *Block) Producer() library.AccountName		{ return b.header.Producer }
+
+func (b *Block) Header() *Header				{ return copyHeader(b.header) }
+
+func (b *Block) Transactions() []*Transaction 	{ return b.transactions }
