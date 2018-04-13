67535228aad1a042e3ecb1b64dd72eb25716ea91
luomeiqin <maypen890@aliyun.com>
2018-04-13 19:15:15 +0800

    add uint128 test

---
+++ common/uint128_test.go
@@ -0,0 +1,35 @@
+// Copyright 2017~2022 The Bottos Authors
+// This file is part of the Bottos Chain library.
+// Created by Rocket Core Team of Bottos.
+
+//This program is free software: you can distribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+// along with bottos.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * file description:  file introduction for commom tip
+ * @Author: May Luo
+ * @Date:   2017-12-01
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+package common
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestUint128_MaxUint128(t *testing.T) {
+	value := MaxUint128()
+	fmt.Println(value)
+}
