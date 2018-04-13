5dac3c7abd6853f6f424bbce43893e38f87c7152
luomeiqin <maypen890@aliyun.com>
2018-04-13 11:32:28 +0800

    update default

---
+++ config/default-value.go
@@ -0,0 +1,33 @@
+// Copyright 2017~2022 The Bottos Authors
+// This file is part of the Bottos Chain library.
+// Created by Rocket Core Team of Bottos.
+
+//This program is free software: you can distribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+// along with bottos.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * file description:  file introduction for commom tip
+ * @Author: May Luo
+ * @Date:   2017-12-01
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+
+package config
+
+const DEFAULT_BLOCK_INTERVAL uint32 = 3
+const BLOCK_PER_ROUND uint32 = 21
+const VOTED_PRODUCERS_PER_ROUND uint32 = 20
+const IRREVERSIBLE_THRESHOLD_PERCENT uint32 = 70
+const MAX_PRODUCER_VOTES uint32 = 30
+const PRODUCER_PATICIPATION uint32 = 33
