af4483c3895532eebfc77fd8f052745dde6427a3
Gong Zibin <gzb1985@gmail.com>
2018-04-16 11:50:01 +0800

    block refactor

--- common/types/block.go
+++ common/types/block.go
@@ -6,29 +6,15 @@ import (
 	"bytes"
 	"io"
 	"crypto/sha256"
+
+	proto "github.com/golang/protobuf/proto"
 )
-
-type Block struct {
-	header			*Header
-	transactions	[]*Transaction
-}
-
-type Header struct {
-	PrevBlockHash	Hash		// Hash of Previos block
-	Number      	uint32				// Block Number
-	Timestamp       uint32         		// Creation time
-	MerkleRoot		Hash
-	Producer		AccountName
-	ProducerChange	[]AccountName
-	ProducerSign	Hash	// TODO ECSDA sign type
-}
 
 func NewBlock(h *Header, txs []*Transaction) *Block {
 	b := Block{header: copyHeader(h)}
 
 	if len(txs) == 0 {
 	} else {
-		// TODO Compute Hash
 		b.transactions = make([]*Transaction, len(txs))
 		copy(b.transactions, txs)
 	}
@@ -36,43 +22,14 @@ func NewBlock(h *Header, txs []*Transaction) *Block {
 	return &b
 }
 
-func (h *Header) Serialize(w io.Writer) error {
-
-	return nil
-}
-
-func (h *Header) Deserialize(r io.Reader) error {
-
-
-	return nil
-}
-
-func (b *Block) Serialize(w io.Writer) error {
-	b.header.Serialize(w)
-
-	return nil
-}
-
-
-func (b *Block) Deserialize(r io.Reader) error {
-	var header Header
-	err := header.Deserialize(r)
-	if err != nil {
-		return fmt.Errorf("Header Deserialize failed: %s", err)
-	}
-
-	// TODO
-
-	b.header = &header
-	return nil
-}
-
 func (b *Block) Hash() Hash {
 	return b.header.Hash()
 }
 
 func (h *Header) Hash() Hash {
-	return Hash{}
+	data, _ := proto.Marshal(h)
+	h := sha256.Sum256(data)
+	return h
 }
 
 func copyHeader(h *Header) *Header {
@@ -83,12 +40,34 @@ func copyHeader(h *Header) *Header {
 	return &cpy
 }
 
-func (b *Block) PrevBlockHash() Hash		{ return b.header.PrevBlockHash }
-func (b *Block) Number() uint32     		{ return b.header.Number }
-func (b *Block) Time() uint32				{ return b.header.Timestamp }
-func (b *Block) MerkleRoot() Hash			{ return b.header.MerkleRoot }
-func (b *Block) Producer() AccountName		{ return b.header.Producer }
+func (b *Block) GetPrevBlockHash() Hash {
+	bh := b.GetHeader().GetPrevBlockHash()
+	return BytesToHash(bh)
+}
 
-func (b *Block) Header() *Header				{ return copyHeader(b.header) }
+func (b *Block) GetNumber() uint32 { 
+	return b.GetHeader().GetNumber()
+}
 
-func (b *Block) Transactions() []*Transaction 	{ return b.transactions }
+func (b *Block) GetTimestamp() uint64 { 
+	return b.GetHeader().GetTimestamp()
+}
+
+func (b *Block) GetMerkleRoot() Hash {
+	bh := b.GetHeader().GetMerkleRoot()
+	return BytesToHash(bh)
+}
+
+// TODO AccountName Type
+func (b *Block) GetProducer() []byte {
+	return b.GetHeader().GetProducer()
+}
+
+//func (b *Block) GetProducerChange() AccountName {
+//	return b.header.Producer
+//}
+
+func (b *Block) GetProducerSign() Hash {
+	bh := b.GetHeader().GetProducerSign()
+	return BytesToHash(bh)
+}
