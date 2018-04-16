53d1be4d284c6bcf45090e06a875a30edebf6807
Gong Zibin <gzb1985@gmail.com>
2018-04-16 14:10:50 +0800

    add blockchain cache

--- common/blockchain.go
+++ common/blockchain.go
@@ -1,42 +1,57 @@
+// Copyright 2017~2022 The Bottos Authors
+// This file is part of the Bottos Chain library.
+// Created by Rocket Core Team of Bottos.
+
+//This program is free software: you can distribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+// along with bottos.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * file description:  general Hash type
+ * @Author: Gong Zibin
+ * @Date:   2017-12-07
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+
 package common
 
 import (
 	"fmt"
 	"sync"
 	"github.com/bottos-project/core/common/types"
-
-	"github.com/hashicorp/golang-lru"
-)
-
-const (
-	blockCacheLimit     = 256
-	checkpointLimit     = 200
 )
 
 type BlockChain struct {
 	blockDb			Database
+	blockCache		*BlockChainCache
 	//stateDb		Database
-	//extraDb		Database
 
 	genesisBlock 	*types.Block
 
 	mu				sync.RWMutex
 	chainmu			sync.RWMutex
-
-	checkpoint      int // TODO
-	currentBlock	*types.Block
-	lastBlockHash   Hash
-
-	cache           *lru.Cache
 }
 
 func CreateBlockChain(blockDb Database) (*BlockChain, error) {
-	cache, _ := lru.New(blockCacheLimit)
+	blockCache, err := CreateBlockChainCache()
+	if err != nil {
+		return nil, err
+	}
+
 	bc := &BlockChain{
 		blockDb:  blockDb,
+		blockCache: blockCache,
 		//stateDb:  stateDb,
-		//extraDb:  extraDb,
-		cache:    cache,
 	}
 
 	bc.genesisBlock = bc.GetBlockByNumber(0)
@@ -48,12 +63,13 @@ func CreateBlockChain(blockDb Database) (*BlockChain, error) {
 		}
 	}
 
-	err := bc.loadLastState()
+	err := bc.loadBlockDb()
 	if err != nil {
 		return nil, err
 	}
 
-	bc.makeCache()
+	// init block cache
+	bc.initBlockCache()
 
 	return bc, nil
 }
@@ -63,24 +79,21 @@ func (bc *BlockChain) GetGenesisBlock() *types.Block {
 }
 
 func (bc *BlockChain) HasBlock(hash Hash) bool {
-	if bc.cache.Contains(hash) {
+	if bc.blockCache.HasBlock(hash) {
 		return true
 	}
 
-	data, _ := bc.blockDb.Get(append(blockHashPre, hash[:]...))
-	return len(data) != 0
+	retrun HasBlock(bc.blockDb, hash)
 }
 
 func (bc *BlockChain) GetBlock(hash Hash) *types.Block {
-	if block, ok := bc.cache.Get(hash); ok {
-		return block.(*types.Block)
+	// cache
+	block := bc.blockCache.GetBlock(hash)
+	if block != nil {
+		return block
 	}
-	block := GetBlock(bc.blockDb, hash)
-	if block == nil {
-		return nil
-	}
-	bc.cache.Add(block.Hash(), block)
-	return block
+
+	return GetBlock(bc.blockDb, hash)
 }
 
 func (bc *BlockChain) GetBlockByHash(hash Hash) *types.Block {
@@ -88,8 +101,13 @@ func (bc *BlockChain) GetBlockByHash(hash Hash) *types.Block {
 }
 
 func (bc *BlockChain) GetBlockByNumber(number uint32) *types.Block {
+	block := bc.blockCache.GetBlockByNum(number)
+	if block != nil {
+		return block
+	}
+
 	hash := GetBlockHashByNumber(bc.blockDb, number)
-	if hash == (Hash{}) {
+	if hash == (library.Hash{}) {
 		return nil
 	}
 	return bc.GetBlock(hash)
@@ -99,95 +117,207 @@ func (bc *BlockChain) GetBlockHashByNumber(number uint32) Hash {
 	return GetBlockHashByNumber(bc.blockDb, number)
 }
 
+func (bc *BlockChain) WriteBlock(block *types.Block) error {
+	return WriteBlock(bc.blockDb, block)
+}
+
+func (bc *BlockChain) HeadBlockTime() uint64 {
+	dpo := bc.stateDb.GetDynamicGlobalPropertyObject()
+	return dpo.Time
+}
+
+func (bc *BlockChain) HeadBlockNum() uint32 {
+	dpo := bc.stateDb.GetDynamicGlobalPropertyObject()
+	return dpo.HeadBlockNum
+}
+
+func (bc *BlockChain) HeadBlockHash() Hash {
+	dpo := bc.stateDb.GetDynamicGlobalPropertyObject()
+	return dpo.HeadBlockHash
+}
+
+func (bc *BlockChain) HeaderBlockProducer() AccountName {
+	dpo := bc.stateDb.GetDynamicGlobalPropertyObject()
+	return dpo.CurrentProducer
+}
+
 
 // internal
-func (bc *BlockChain) loadLastState() error {
-	data, _ := bc.blockDb.Get([]byte("LastBlock"))
-	if len(data) != 0 {
-		block := bc.GetBlockByHash(BytesToHash(data))
-		if block != nil {
-			bc.currentBlock = block
-			bc.lastBlockHash = block.Hash()
+func (bc *BlockChain) getBlockDbLastBlock() *types.Block {
+	return GetLastBlock(bc.blockDb)
+}
 
-			fmt.Printf("current block num = %v\n", bc.currentBlock.Number())
-		} else {
-			if bc.recover() {
+func (bc *BlockChain) initBlockCache() error {
+
+	// TODO 疑问
+	block := bc.getBlockDbLastBlock()
+	if block != nil {
+		_, err := bc.blockCache.Insert(block)
+		return err
+	}
+
+	return nil
+}
+
+func (bc *BlockChain) LoadBlockDb() error {
+	lastBlock := GetLastBlock(bc.blockDb)
+	if lastBlock == nil {
+		// TODO blockDb Recover()
+		return Errorf("Loading block database fail, try recovering")
+	}
+
+	bc.blockCache.Insert(lastBlock)
+
+	// TODO  for testing
+	bc.updateGlobalDynamicProperty(lastBlock)
+	dpo := bc.stateDb.GetDynamicGlobalPropertyObject()
+	dpo.LastIrreversibleBlockNum = lastBlock.Number()
+	bc.stateDb.SetDynamicGlobalPropertyObject(dpo)
+	fmt.Printf("current block num = %v, hash = %v\n", lastBlock.Number(), bc.lastBlockHash)
+
+	// TODO
+	if HeadBlockNum() < lastBlock.GetNumber() {
+		// LoadAndExcuteBlocks()
+	}
+
+	return nil
+}
+
+// TODO 
+func (bc *BlockChain) updateGlobalProperty(block *types.Block) {
+}
+
+// TODO 
+func (bc *BlockChain) updateGlobalDynamicProperty(block *types.Block) {
+	dpo := bc.stateDb.GetDynamicGlobalPropertyObject()
+	dpo.HeadBlockNum = block.Number()
+	dpo.HeadBlockHash = block.Hash()
+	dpo.Time = block.Time()
+	dpo.CurrentProducer = block.Producer()
+	bc.stateDb.SetDynamicGlobalPropertyObject(dpo)
+}
+
+// TODO 
+func (bc *BlockChain) updateLastIrreversibleBlock(block *types.Block) {
+	// TODO  compute new LIB
+	dpo := bc.stateDb.GetDynamicGlobalPropertyObject()
+	// for test
+	if dpo.HeadBlockNum > dpo.LastIrreversibleBlockNum + 7 {
+		dpo.LastIrreversibleBlockNum = dpo.HeadBlockNum - 7
+	}
+	bc.stateDb.SetDynamicGlobalPropertyObject(dpo)
+
+	// write LIB to blockDb
+	newLIB := dpo.LastIrreversibleBlockNum
+	lastBlockNum := uint32(0)
+	lastBlock := bc.getBlockDbLastBlock()
+	if lastBlock != nil {
+		lastBlockNum = lastBlock.Number()
+	}
+
+	fmt.Printf("lastBlockNum = %v, newLIB = %v\n", lastBlockNum, newLIB)
+
+	if lastBlockNum < newLIB {
+		for i := lastBlockNum + 1; i <= newLIB; i++ {
+			block := bc.GetBlockByNumber(i)
+			if block != nil {
+				bc.WriteBlock(block)
 			} else {
-			}
+				fmt.Printf("block num = %v not found\n", i)
+			}	
 		}
-	} else {
-		bc.Reset()
+	}
+
+	// trim blockCache
+	bc.blockCache.Trim(dpo.HeadBlockNum, newLIB)
+}
+
+
+func (bc *BlockChain) ValidateBlockPrevHash(block *types.Block) error {
+	if block.PrevBlockHash() != bc.HeadBlockHash() {
+		return fmt.Errorf("Block Prev Hash error, head block Hash=%x, block PrevBlockHash=%x", bc.HeadBlockHash(), block.PrevBlockHash())
 	}
 
 	return nil
 }
 
-func (bc *BlockChain) recover() bool{
-	fmt.Println("TODO: BlockChain recover")
-	return true
+func (bc *BlockChain) ValidateBlockHeader(block *types.Block) error {
+	// block timestamp check
+	if block.Time() <= bc.HeadBlockTime() {
+		return fmt.Errorf("Block Timestamp error, head block time=%v, block time=%v", bc.HeadBlockTime(), block.Time())
+	}
+
+	if block.Time() > bc.HeadBlockTime() + uint64(consensus.GetBlockInterval()) {
+		return fmt.Errorf("Block Timestamp error, head block time=%v, block time=%v", bc.HeadBlockTime(), block.Time())
+	}
+
+	// TODO producer_change check
+	// ...
+
+	// TODO producer signature check
+	//slot := store.GetSlotAtTime(block.Time())
+	//producerName := store.GetScheduledProducer(slot)
+	//scheduleProducerObj := bc.stateDb.GetProducerObject(producerName)
+	if ok := block.ValidateSign(/*producer*/); !ok {
+		return fmt.Errorf("Producer Sign Error")
+	}
+
+	// producer schedule check
+	//blockProducer := block.Producer()
+	//if string(blockProducer[:]) != scheduleProducerObj.Owner {
+	//	return fmt.Errorf("Producer Producer Error")
+	//}
+
+	return nil
 }
 
-func (bc *BlockChain) Reset() {
-	fmt.Println("TODO: BlockChain reset")
-}
-
-func (bc *BlockChain) insert(block *types.Block) {
-	err := WriteHead(bc.blockDb, block)
+func (bc *BlockChain) ExecuteBlock(block *types.Block) error {
+	// full validate
+	err := bc.ValidateBlockPrevHash(block)
 	if err != nil {
+		fmt.Printf("ValidateBlockPrevHash error %v\n", err)
+		return err
+	}
+	err = bc.ValidateBlockHeader(block)
+	if err != nil {
+		fmt.Printf("ValidateBlockHeader error %v\n", err)
+		return HeaderValidateErr
 	}
 
-	bc.currentBlock = block
-	bc.lastBlockHash = block.Hash()
-}
+	// TODO excute block
+	fmt.Println("BlockChain : Executing block")
 
-
-func (bc *BlockChain) makeCache() {
-	bc.cache, _ = lru.New(blockCacheLimit)
-	bc.cache.Add(bc.genesisBlock.Hash(), bc.genesisBlock)
-	for _, block := range bc.GetBlocksFromHash(bc.currentBlock.Hash(), blockCacheLimit) {
-		bc.cache.Add(block.Hash(), block)
-	}
-}
-
-func (bc *BlockChain) GetBlocksFromHash(hash Hash, n int) (blocks []*types.Block) {
-	for i := 0; i < n; i++ {
-		block := bc.GetBlockByHash(hash)
-		if block == nil {
-			break
-		}
-		blocks = append(blocks, block)
-		hash = block.PrevBlockHash()
-	}
-	return
-}
-
-
-func (bc *BlockChain) ApplyBlock(block *types.Block) error {
-	fmt.Println("Apply block")
+	// update global property
+	bc.updateGlobalProperty(block)
+	bc.updateGlobalDynamicProperty(block)
+	bc.updateLastIrreversibleBlock(block)
 
 	return nil
 }
 
-func (bc *BlockChain) PushBlock(block *types.Block) error {
+func (bc *BlockChain) InsertBlock(block *types.Block) error {
 	bc.chainmu.Lock()
 	defer bc.chainmu.Unlock()
 
-	fmt.Println("BlockChain : push block")
-
-	// TODO: validate
-
-	// TODO record stateDb revision
-	err := bc.ApplyBlock(block)
-	if (err != nil) {
-		// TODO restore stateDb revision
+	// push to cache, block must link now, TODO: fork process
+	_, err := bc.blockCache.Insert(block)
+	if err != nil {
+		fmt.Printf("blockCache insert error %v", err)
+		return err
 	}
 
+	// TODO record stateDb revision
+	err = bc.ExecuteBlock(block)
+	if (err != nil) {
+		// TODO restore stateDb revision
+		fmt.Printf("InsertBlock error %v", err)
+		return err
+	}
 	// TODO commit stateDb revision
-
-	// TODO self.blockDB.Put()
-	bc.currentBlock = block
 	
 	return nil
 }
+
+
 
 
