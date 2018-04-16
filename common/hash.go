793381d11e5ebd1454cdce6a53b6dfa89ae6f052
Gong Zibin <gzb1985@gmail.com>
2018-04-16 11:49:32 +0800

    hash type definition

---
+++ common/hash.go
@@ -0,0 +1,114 @@
+// Copyright 2017~2022 The Bottos Authors
+// This file is part of the Bottos Chain library.
+// Created by Rocket Core Team of Bottos.
+
+//This program is free software: you can distribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+// along with bottos.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * file description:  general Hash type
+ * @Author: Gong Zibin
+ * @Date:   2017-12-05
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+ package common
+
+ import (
+	"bytes"
+	"encoding/hex"
+ )
+ 
+const (
+	HashLength = 32
+)
+
+type (
+	Hash [HashLength]byte
+)
+
+func BytesToHash(b []byte) Hash {
+	var h Hash
+	h.SetBytes(b)
+	return h
+}
+
+func StringToHash(s string) Hash {
+	return BytesToHash([]byte(s))
+}
+
+func EmptyHash(h Hash) bool {
+	return h == Hash{}
+}
+
+func HexToHash(s string) Hash {
+	return BytesToHash(FromHex(s))
+}
+
+func (h Hash) ToString() string {
+	return string(h[:])
+}
+
+func (h Hash) Bytes() []byte {
+	return h[:]
+}
+
+func (h Hash) ToHexString() string {
+	return Bytes2Hex(h[:])
+}
+
+func (h *Hash) SetString(s string) {
+	h.SetBytes([]byte(s)) 
+}
+
+func (h *Hash) SetBytes(b []byte) {
+	if len(b) > len(h) {
+		b = b[len(b)-HashLength:]
+	}
+
+	copy(h[HashLength-len(b):], b)
+}
+
+func BytesToHex(d []byte) string {
+	return hex.EncodeToString(d)
+}
+
+func HexToBytes(str string) []byte {
+	h, _ := hex.DecodeString(str)
+
+	return h
+}
+
+func NumberToBytes(num interface{}, bits int) []byte {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, num)
+	if err != nil {
+		fmt.Println("NumberToBytes failed:", err)
+	}
+
+	return buf.Bytes()[buf.Len()-(bits/8):]
+}
+
+func HexStringToBytes(s string) []byte {
+	if len(s) > 1 {
+		if s[0:2] == "0x" {
+			s = s[2:]
+		}
+		if len(s)%2 == 1 {
+			s = "0" + s
+		}
+		return HexToBytes(s)
+	}
+	return nil
+}
+
