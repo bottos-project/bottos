004efa68ee550ae054df6ce3a17acdd598186c9d
bottosgithub <zhangshaokui@bottos.org>
2018-04-17 20:56:19 +0800

    add actor

---
+++ action/actor/actor.go
@@ -0,0 +1,71 @@
+// Copyright 2017~2022 The Bottos Authors
+// This file is part of the Bottos Chain library.
+// Created by Rocket Core Team of Bottos.
+
+//This program is free software: you can distribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+// along with bottos.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * file description:  actor entry
+ * @Author:
+ * @Date:   2017-12-06
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+
+package actionactor
+
+import (
+	"fmt"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+	apiactor "github.com/bottos-project/core/action/actor/api"
+	blockactor "github.com/bottos-project/core/action/actor/block"
+	netactor "github.com/bottos-project/core/action/actor/net"
+	produceractor "github.com/bottos-project/core/action/actor/producer"
+	trxactor "github.com/bottos-project/core/action/actor/transaction"
+)
+
+var apiActorPid *actor.PID
+var netActorPid *actor.PID
+var trxActorPid *actor.PID
+var blockActorPid *actor.PID
+var producerActorPid *actor.PID
+
+func InitActors() {
+
+	fmt.Println("InitActors")
+
+	apiActorPid = apiactor.NewApiActor()
+
+	netActorPid = netactor.NewNetActor()
+
+	trxActorPid = trxactor.NewTrxActor()
+
+	blockActorPid = blockactor.NewBlockActor()
+
+	producerActorPid = produceractor.NewProducerActor()
+
+	RegisterActorMsgTbl()
+
+}
+
+func RegisterActorMsgTbl() {
+
+	fmt.Println("RegisterActorMsgTbl")
+
+	apiactor.SetTrxActorPid(trxActorPid) // api --> trx
+
+	trxactor.SetApiActorPid(apiActorPid) // trx --> api
+
+}
