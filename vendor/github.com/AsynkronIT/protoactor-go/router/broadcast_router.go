bb2597239d1c7c106b507558024da325e9a50d26
bottosgithub <zhangshaokui@bottos.org>
2018-04-17 21:37:05 +0800

    add proto actor

---
+++ vendor/github.com/AsynkronIT/protoactor-go/router/broadcast_router.go
@@ -0,0 +1,55 @@
+package router
+
+import (
+	"sync/atomic"
+	"unsafe"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+)
+
+type broadcastGroupRouter struct {
+	GroupRouter
+}
+
+type broadcastPoolRouter struct {
+	PoolRouter
+}
+
+type broadcastRouterState struct {
+	routees *actor.PIDSet
+}
+
+func (state *broadcastRouterState) SetRoutees(routees *actor.PIDSet) {
+	rts := *routees
+	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&state.routees)), unsafe.Pointer(&rts))
+}
+
+func (state *broadcastRouterState) GetRoutees() *actor.PIDSet {
+	newSet := &actor.PIDSet{}
+	state.routees.ForEach(func(i int, pid actor.PID) {
+		newSet.Add(&pid)
+	})
+	return newSet
+}
+
+func (state *broadcastRouterState) RouteMessage(message interface{}) {
+	state.routees.ForEach(func(i int, pid actor.PID) {
+		pid.Tell(message)
+	})
+}
+
+func NewBroadcastPool(size int) *actor.Props {
+	return actor.FromSpawnFunc(spawner(&broadcastPoolRouter{PoolRouter{PoolSize: size}}))
+}
+
+func NewBroadcastGroup(routees ...*actor.PID) *actor.Props {
+	return actor.FromSpawnFunc(spawner(&broadcastGroupRouter{GroupRouter{Routees: actor.NewPIDSet(routees...)}}))
+}
+
+func (config *broadcastPoolRouter) CreateRouterState() Interface {
+	return &broadcastRouterState{}
+}
+
+func (config *broadcastGroupRouter) CreateRouterState() Interface {
+	return &broadcastRouterState{}
+}
