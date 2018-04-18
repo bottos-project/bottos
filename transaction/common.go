d35b250d086461e63586b33a7c57ae8a74009e8b
zhangwei <zhangwei@bottos.org>
2018-04-18 16:44:50 +0800

    add commn process: apply transaction, which will used by push trx,produce block and apply block

---
+++ transaction/common.go
@@ -0,0 +1,13 @@
+package transaction
+
+import (
+	"github.com/bottos-project/core/common/types"
+)
+
+
+func ApplyTransaction(trx *types.Transaction) {
+
+	/* save to db */
+
+	/* call evm... */
+}
