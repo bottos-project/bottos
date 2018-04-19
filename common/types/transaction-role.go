8728288ee16604e1dff09617b53865386328fcea
zhangwei <zhangwei@bottos.org>
2018-04-19 22:12:14 +0800

    add trx hash calc func

--- common/types/transaction-role.go
+++ common/types/transaction-role.go
@@ -1,9 +1,19 @@
 package types 
 
 import (
+	"crypto/sha256"
 	"github.com/bottos-project/core/common"
+	"github.com/golang/protobuf/proto"
 )
 
-func (tx *Transaction) Hash() common.Hash {
-	return common.Hash{}
+func (trx *Transaction) Hash() common.Hash {
+	data, _ := proto.Marshal(trx)
+	hash := sha256.Sum256(data)
+	return hash
+}
+
+
+
+func (trx *Transaction) ValidateSign() bool {
+	return true
 }
