09e8da9dac8b2a0a8348f53c9d98b7f58cf64726
zhangwei <zhangwei@bottos.org>
2018-04-19 14:34:33 +0800

    modify generate proto rpc interface  for push transaction

--- tool/pb-gen.bat
+++ tool/pb-gen.bat
@@ -1,5 +1,8 @@
 rem generate common/types
 protoc\bin\protoc.exe -I ..\..\..\..\ --go_out=plugins=micro:..\..\..\..\   ..\..\..\..\github.com\bottos-project\core\common\types\transaction.proto ..\..\..\..\github.com\bottos-project\core\common\types\block.proto
 
+rem generate api
+protoc\bin\protoc.exe -I ..\..\..\..\ --go_out=plugins=micro:..\..\..\..\   ..\..\..\..\github.com\bottos-project\core\api\core-api.proto
+
 rem generate tool/example
 protoc\bin\protoc.exe -I ..\..\..\..\ --go_out=plugins=micro:..\..\..\..\   ..\..\..\..\github.com\bottos-project\core\tool\example\example.proto
