3caf509d5bd4c47d2b95ff25ae83a2ef7cdba824
zhangwei <zhangwei@bottos.org>
2018-04-21 15:12:47 +0800

    modify script of generate protoc go file

--- tool/pb-gen.sh
+++ tool/pb-gen.sh
@@ -1,2 +1,2 @@
 protoc -I ../../../../ --go_out=plugins=micro:../../../../   ../../../../github.com/bottos-project/core/common/types/transaction.proto ../../../../github.com/bottos-project/core/common/types/block.proto 
-protoc -I ../../../../ --go_out=plugins=micro:../../../../ ../../../../github.com/bottos-project/core/api/core-api.proto
+protoc -I ../../../../ --go_out=plugins=micro:../../../../   ../../../../github.com/bottos-project/core/api/core-api.proto
