c676af395e6957c55c59b4e9b635df6d6a323311
zhangwei <zhangwei@bottos.org>
2018-04-21 20:00:30 +0800

    move function handler

--- transaction/common.go
+++ transaction/common.go
@@ -1,36 +1,32 @@
 package transaction
 
 import (
-	"github.com/bottos-project/core/common"
+	//"github.com/bottos-project/core/common"
 	"github.com/bottos-project/core/common/types"
 )
 
 
+func CheckTransactionLifeTime(trx *types.Transaction) { 
+
+}
+
+func CheckTransactionUnique(trx *types.Transaction) { 
+
+}
+
+
+func CheckTransactionMatchChain(trx *types.Transaction) { 
+
+}
+
 func ApplyTransaction(trx *types.Transaction) {
 
+	CheckTransactionLifeTime(trx)
+	CheckTransactionUnique(trx)
+	CheckTransactionMatchChain(trx)
 	/* save to db */
 
 	/* call evm... */
 }
 
 
-func GetAllPendingTransactions() ([]*types.Transaction){
-
-	return nil;
-}
-
-
-func RemoveTransactions(trxs []*types.Transaction){
-
-}
-
-
-func RemoveSingleTransaction(*types.Transaction){
-
-}
-
-
-func GetPendingTransaction(trxHash common.Hash) *types.Transaction {	
-
-	return nil;
-}
