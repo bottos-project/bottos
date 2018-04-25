afe07f8ca7cc89386e1388a4b3b11485fecef055
zhangwei <zhangwei@bottos.org>
2018-04-25 10:56:53 +0800

    add check of transaction uniqueue

--- transaction/trx-apply.go
+++ transaction/trx-apply.go
@@ -28,17 +28,22 @@ func GetTrxApplyService() *TrxApplyService {
 	return trxApplyServiceInst
 }
 
-func (trxApplyService *TrxApplyService) CheckTransactionLifeTime(trx *types.Transaction) { 
-
+func (trxApplyService *TrxApplyService) CheckTransactionLifeTime(trx *types.Transaction) bool { 
+	return true
 }
 
-func (trxApplyService *TrxApplyService) CheckTransactionUnique(trx *types.Transaction) { 
+func (trxApplyService *TrxApplyService) CheckTransactionUnique(trx *types.Transaction) bool { 	
+	transactionExpiration, _ := role.GetTransactionExpirationObjectByHash(trxApplyService.stateDb, trx.Hash())
+	if (nil != transactionExpiration) {
+		return false
+	}
 
+	return true
 }
 
 
-func (trxApplyService *TrxApplyService) CheckTransactionMatchChain(trx *types.Transaction) { 
-
+func (trxApplyService *TrxApplyService) CheckTransactionMatchChain(trx *types.Transaction) bool { 
+	return true
 }
 
 func (trxApplyService *TrxApplyService) SaveTransactionExpiration(trx *types.Transaction) { 
@@ -46,16 +51,24 @@ func (trxApplyService *TrxApplyService) SaveTransactionExpiration(trx *types.Tra
     role.SetTransactionExpirationObjectRole(trxApplyService.stateDb, trx.Hash(), transactionExpiration)
 }
 
-func (trxApplyService *TrxApplyService)ApplyTransaction(trx *types.Transaction) error {
+func (trxApplyService *TrxApplyService)ApplyTransaction(trx *types.Transaction) (bool, error) {
 
 	/* check account validate,include contract account */
 	/* check signature */
-	trxApplyService.CheckTransactionLifeTime(trx)
-	trxApplyService.CheckTransactionUnique(trx)
-	trxApplyService.CheckTransactionMatchChain(trx)
+	if (!trxApplyService.CheckTransactionLifeTime(trx)) {
+		return false, nil
+	}
+
+	if (!trxApplyService.CheckTransactionUnique(trx)) {
+		return false, nil
+	}
+	
+	if (!trxApplyService.CheckTransactionMatchChain(trx)) {
+		return false, nil
+	}
 	trxApplyService.SaveTransactionExpiration(trx)
 
 	/* call evm... */
 
-	return nil
+	return true , nil
 }
