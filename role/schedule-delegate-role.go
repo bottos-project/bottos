fd0861f7f8454d0156066173b22093346efa967f
luomeiqin <maypen890@aliyun.com>
2018-04-27 16:30:55 +0800

    update producer

--- role/schedule-delegate-role.go
+++ role/schedule-delegate-role.go
@@ -12,13 +12,16 @@ func GetScheduleDelegateRole(ldb *db.DBService, slotNum uint32) (string, error) 
 		fmt.Println("err")
 		return "", err
 	}
+	fmt.Println("currentSlotNum", chainObject.CurrentAbsoluteSlot, slotNum)
 	currentSlotNum := chainObject.CurrentAbsoluteSlot + uint64(slotNum)
 	currentCoreState, err := GetGlobalPropertyRole(ldb)
+	fmt.Println("currentSlotNum", currentSlotNum)
 	if err != nil {
 		fmt.Println("err")
 		return "", err
 	}
 	size := uint64(len(currentCoreState.CurrentDelegates))
+	fmt.Println("size", size)
 	accountName := currentCoreState.CurrentDelegates[currentSlotNum%size]
 	return accountName, nil
 
