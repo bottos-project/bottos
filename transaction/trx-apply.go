217a376de767438a12416aebd856abc15dfcd283
zhangwei <zhangwei@bottos.org>
2018-04-28 16:32:08 +0800

    add check trx lifetime

--- transaction/trx-apply.go
+++ transaction/trx-apply.go
@@ -1,23 +1,29 @@
 package transaction
 
 import (
+	"github.com/bottos-project/core/action/env"
 	"sync"
 	"fmt"
+	"time"
 
 	"github.com/bottos-project/core/common/types"
+	"github.com/bottos-project/core/db"
 	"github.com/bottos-project/core/role"
+	"github.com/bottos-project/core/config"
+	"github.com/bottos-project/core/chain"
 )
 
 type TrxApplyService struct {
 	roleIntf role.RoleInterface
+        core        chain.BlockChainInterface
 }
 
 var trxApplyServiceInst *TrxApplyService
 var once sync.Once
 
-func CreateTrxApplyService(roleIntf role.RoleInterface) *TrxApplyService {
+func CreateTrxApplyService(env *env.ActorEnv) *TrxApplyService {
 	once.Do(func() {
-		trxApplyServiceInst = &TrxApplyService{roleIntf: roleIntf}
+		trxApplyServiceInst = &TrxApplyService{roleIntf: env.RoleIntf, core: env.Chain}
 	})
 
 	return trxApplyServiceInst
@@ -28,12 +34,33 @@ func GetTrxApplyService() *TrxApplyService {
 }
 
 func (trxApplyService *TrxApplyService) CheckTransactionLifeTime(trx *types.Transaction) bool {
+	
+	curTime := trxApplyService.core.HeadBlockTime()
+
+	//for test:
+	trx.Lifetime = curTime  + 600
+
+	if (curTime >= trx.Lifetime) {
+		fmt.Println("lifetime ", time.Unix((int64)(trx.Lifetime), 0),"have past, head time ", time.Unix((int64)(curTime), 0), "trx hash: ", trx.Hash())
+		return false
+	}
+
+	
+
+	if (trx.Lifetime >= (curTime + config.DEFAULT_MAX_LIFE_TIME)) {
+		fmt.Println("lifetime ", time.Unix((int64)(trx.Lifetime), 0),"too far, head time ", time.Unix((int64)(curTime), 0), "trx hash: ", trx.Hash())
+		return false
+	}
+
 	return true
 }
 
 func (trxApplyService *TrxApplyService) CheckTransactionUnique(trx *types.Transaction) bool {
 	transactionExpiration, _ := trxApplyService.roleIntf.GetTransactionExpiration(trx.Hash())
 	if nil != transactionExpiration {
+		fmt.Println("check unique error ", trx.Hash())
+		fmt.Println("transactionExpiration is  ", transactionExpiration)
+
 		return false
 	}
 
@@ -41,6 +68,14 @@ func (trxApplyService *TrxApplyService) CheckTransactionUnique(trx *types.Transa
 }
 
 func (trxApplyService *TrxApplyService) CheckTransactionMatchChain(trx *types.Transaction) bool {
+
+
+
+	//blockHistory, _ = role.GetBlockHistoryByNumber(stateDb, trx.Cursor)
+	//if (nil != blockHistory) && blockHistory.BlockHash == trx.CursorLabel) {
+	//	return true
+	//}
+
 	return true
 }
 
