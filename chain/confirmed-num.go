3ca3c9d8bec36fe6a1fbbc6b15cc5ba3fbbf9c59
Gong Zibin <gzb1985@gmail.com>
2018-05-09 23:50:06 +0800

    index bound check

--- chain/confirmed-num.go
+++ chain/confirmed-num.go
@@ -31,6 +31,19 @@
 
 type ConfirmedNum []uint32
 
-func (c ConfirmedNum) Len() int           { return len(c) }
-func (c ConfirmedNum) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c ConfirmedNum) Less(i, j int) bool { return c[i] < c[j] }
+func (c ConfirmedNum) Len() int { 
+	return len(c) 
+}
+
+func (c ConfirmedNum) Swap(i, j int) { 
+	if i >= 0 && i < c.Len() && j >= 0 && j < c.Len() {
+		c[i], c[j] = c[j], c[i] 
+	}
+}
+
+func (c ConfirmedNum) Less(i, j int) bool { 
+	if i >= 0 && i < c.Len() && j >= 0 && j < c.Len() {
+		return c[i] < c[j] 
+	}
+	return false
+}
