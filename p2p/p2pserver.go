1d4833c549d96f38cf279b78ac4f9818d68148db
iwojima <iwojima@163.com>
2018-05-19 15:42:48 +0800

    [P2P] update codes

--- p2p/p2pserver.go
+++ p2p/p2pserver.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"sync"
 	"errors"
+	"time"
 	//"log"
 	//"encoding/binary"
 	//"encoding/hex"
@@ -12,10 +13,6 @@ import (
 	"io/ioutil"
 	"github.com/AsynkronIT/protoactor-go/actor"
 
-)
-
-const (
-	CONF_FILE = "config.json"
 )
 
 type netServer struct {
@@ -26,8 +23,12 @@ type netServer struct {
 	listener        net.Listener
 
 	seed_peer       []string
-	neighborNode    []*net.UDPAddr
+
+	neighborList    []*net.UDPAddr
 	serverAddr      *net.UDPAddr
+
+	socket          *net.UDPConn
+
 	nodeMap         map[uint64]*peer
 
 	publicKey       string           //todo
@@ -37,12 +38,14 @@ type netServer struct {
 
 //
 type p2pServer struct{
-	serv       *netServer
-	notify     *notifyManager
+	serv          *netServer
+	notify        *notifyManager
 
-	p2pConfig  *P2PConfig
+	p2pConfig     *P2PConfig
 
-	p2pLock     sync.RWMutex
+	time_interval *time.Timer
+
+	p2pLock        sync.RWMutex
 }
 
 type P2PConfig struct {
@@ -71,7 +74,7 @@ type notifyManager struct {
 	pid      *actor.PID
 
 	//for reading/writing peerlist
-	syncLock sync.RWMutex
+	sync.RWMutex
 }
 
 //parse json configuration
@@ -108,9 +111,34 @@ func NewServ() *p2pServer{
 	p2pconfig := ReadFile(CONF_FILE)
 
 	return &p2pServer{
-		serv:       NewNetServer(p2pconfig),
-		p2pConfig:  p2pconfig,
+		serv:          NewNetServer(p2pconfig),
+		p2pConfig:     p2pconfig,
+		time_interval: time.NewTimer(TIME_INTERVAL * time.Second),
 	}
+}
+
+func (p2p *p2pServer) Init() error {
+
+	fmt.Println("p2pServer::Init()")
+
+	serv_addr := p2p.serv.addr + ":" + fmt.Sprint(p2p.serv.port)
+	var err error
+
+	p2p.serv.serverAddr, err = net.ResolveUDPAddr("udp4", serv_addr)
+	if err != nil {
+		fmt.Println("*ERROR* Failed to Resolve UDP Address !!!")
+		return errors.New("*ERROR* Failed to Resolve UDP Address !!!")
+	}
+
+	//return (*UDPConn, error)
+	p2p.serv.socket, err = net.ListenUDP("udp4", p2p.serv.serverAddr)
+	if err != nil {
+		fmt.Println("*ERROR* Failed to Listen !!! ", err)
+		return errors.New("*ERROR* Failed to Listen !!! ")
+	}
+
+
+	return nil
 }
 
 
@@ -122,26 +150,65 @@ func (p2p *p2pServer) Start() error {
 		return errors.New("*ERROR* P2P Configuration hadn't been inited yet !!!")
 	}
 
+	p2p.Init()
+
 	if p2p.serv != nil {
 		//wait for connection from others
 		p2p.serv.Start()
 	}
 
-	//cconnect to other seed nodes
-	go p2p.ConnectSeed()
+	//Todo connect to other seed nodes
+	go p2p.ActiveSeeds()
 
-	// ping/pong
+	// Todo ping/pong
 	go p2p.RunHeartBeat()
 
 	return nil
 }
 
-//
-func (p2p *p2pServer) ConnectSeed() error {
+func (p2p *p2pServer) ActiveSeeds() error {
+	var i = 0
+	for {
+		select {
+		case <- p2p.time_interval.C:
+			fmt.Println("i = ",i)
+			p2p.ResetTimer()
+		}
+	}
+}
+
+func  (p2p *p2pServer) ResetTimer ()  {
+	p2p.time_interval.Stop()
+	p2p.time_interval.Reset(time.Second * TIME_INTERVAL)
+}
+
+//connect seed during start p2p server
+func (p2p *p2pServer) ConnectSeeds() error {
 	fmt.Println("p2pServer::ConnectSeed()")
+
+	for _ , peer := range p2p.p2pConfig.PeerLst {
+		//fmt.Println(" peer = ",peer)
+		p2p.Connect(peer)  //todo connect remote seed peer , if it's successful , add it into remote_list
+	}
+
 	return nil
 }
 
+//
+func (p2p *p2pServer) Connect(addr string) error {
+	//fmt.Println("p2pServer::ConnectSeed()")
+	remoteAddr, err := net.ResolveUDPAddr("udp4", addr+":"+fmt.Sprint(p2p.serv.port))
+	if err != nil {
+		fmt.Println("*ERROR* Failed to create a remote server addr !!!")
+		return errors.New("*ERROR* Failed to create a remote server addr !!!")
+	}
+
+
+
+	fmt.Println("remoteAddr = ",remoteAddr)
+
+	return nil
+}
 
 //run a heart beat to watch the network status
 func  (p2p *p2pServer) RunHeartBeat() error {
@@ -176,33 +243,24 @@ func (serv *netServer) Listening() {
 
 	fmt.Println("netServer::Listening()")
 
-	userlist := make([]*net.UDPAddr, 0, 10)
-	serv_addr := serv.addr+":"+fmt.Sprint(serv.port)
+	//userlist should be packaged as a "peer" struct
+	userlist  := make([]*net.UDPAddr, 0, 10)
 
-	serverAddr, err := net.ResolveUDPAddr("udp4", serv_addr)
-	if err != nil {
-		fmt.Println("*ERROR* Failed to Resolve UDP Address !!!")
-		return
-	}
-
-	socket, err := net.ListenUDP("udp4", serverAddr) //(*UDPConn, error)
-	if err != nil {
-		fmt.Println("*ERROR* Failed to Listen !!! ", err)
-		return
-	}
 
 	data := make([]byte, 4096)
 	for {
-		read, addr, err := socket.ReadFromUDP(data)
+		read, addr, err := serv.socket.ReadFromUDP(data)
 		if err != nil {
 			fmt.Println("*ERROR* Failed to receive the data : ", err)
 			continue
 		}
 
 		fmt.Println("data = ",data[0:read]," , addr = ",addr)
-		userlist = append(userlist, addr)
+		userlist = append(userlist, addr) //todo set a map[string]*Conn
 		fmt.Println("userlist = ",userlist)
 	}
+
+	return
 }
 
 
@@ -219,6 +277,17 @@ func (notify *notifyManager) Start() {
 
 		//receive
 	}
+}
+
+func (notify *notifyManager) BroadCast(buf []byte, isSync bool) {
+	notify.RLock()
+	defer notify.RUnlock()
+
+	for _ , node := range notify.peerList {
+		fmt.Println("node: ",node)
+	}
+
+	return
 }
 
 //sync trx info with other peer
