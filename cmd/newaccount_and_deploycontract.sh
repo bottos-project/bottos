e894f43d087d482edb02fd659653e15c256368b7
Gong Zibin <gzb1985@gmail.com>
2018-05-29 11:42:51 +0800

    modify command line module name

--- cmd/newaccount_and_deploycontract.sh
+++
@@ -1,7 +0,0 @@
-echo cmd newaccount -name ${1} -pubkey 7QBxKhpppiy7q4AcNYKRY2ofb3mR5RP8ssMAX65VEWjpAgaAnF
-
-./cmd newaccount -name ${1} -pubkey 7QBxKhpppiy7q4AcNYKRY2ofb3mR5RP8ssMAX65VEWjpAgaAnF
-
-echo cmd deploycode -contract ${1} -wasm ./${1}.wasm
-
-./cmd deploycode -contract ${1} -wasm ./${1}.wasm
