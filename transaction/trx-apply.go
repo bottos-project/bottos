b59f78686f1f645a10cf6063f34817f181189293
zhangwei <zhangwei@bottos.org>
2018-05-30 10:44:58 +0800

    add file description

--- transaction/trx-apply.go
+++ transaction/trx-apply.go
@@ -1,3 +1,23 @@
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+// along with bottos.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * file description: apply transaction process
+ * @Author: Wesley
+ * @Date:   2017-12-15
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+
 package transaction
 
 import (
