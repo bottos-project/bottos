ec0237c643b00e1c3c1230fc7e127643b3f0eb0c
luomeiqin <maypen890@aliyun.com>
2018-05-31 08:28:58 +0800

    update producer

--- producer/producer.go
+++ producer/producer.go
@@ -77,7 +77,7 @@ func (p *Reporter) reportBlock(blockTime uint64, accountName string, trxs []*typ
 	// If this block is last in a round, calculate the schedule for the new round
 	if block.Header.Number%config.BLOCKS_PER_ROUND == 0 {
 		newSchedule := p.roleIntf.ElectNextTermDelegates()
-		fmt.Println("next term delgates", newSchedule)
+		//fmt.Println("next term delgates", newSchedule)
 		currentState, err := p.roleIntf.GetCoreState()
 		if err != nil {
 			return nil, err
