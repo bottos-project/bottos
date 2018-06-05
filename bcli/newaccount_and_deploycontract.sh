77ea69553b082e1dea8a656cf4c203b5263bae7e
Gong Zibin <gzb1985@gmail.com>
2018-06-05 17:36:12 +0800

    delete unused files

--- bcli/newaccount_and_deploycontract.sh
+++
@@ -1,7 +0,0 @@
-echo bcli newaccount -name ${1} -pubkey 7QBxKhpppiy7q4AcNYKRY2ofb3mR5RP8ssMAX65VEWjpAgaAnF
-
-./bcli newaccount -name ${1} -pubkey 7QBxKhpppiy7q4AcNYKRY2ofb3mR5RP8ssMAX65VEWjpAgaAnF
-
-echo bcli deploycode -contract ${1} -wasm ./${1}.wasm
-
-./bcli deploycode -contract ${1} -wasm ./${1}.wasm
