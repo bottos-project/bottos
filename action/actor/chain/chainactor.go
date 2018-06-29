a0e3d6f06b32a1517eb60ac2650cfb4922230622
Gong Zibin <gzb1985@gmail.com>
2018-06-29 11:54:28 +0800

    revert net actor response modify

--- action/actor/chain/chainactor.go
+++ action/actor/chain/chainactor.go
@@ -128,15 +128,17 @@ func (c *ChainActor) HandleNewProducedBlock(ctx actor.Context, req *message.Inse
 func (c *ChainActor) HandleReceiveBlock(ctx actor.Context, req *message.ReceiveBlock) {
 	err := actorEnv.Chain.InsertBlock(req.Block)
 
-	resp := &message.ReceiveBlockResp{
-		BlockNum: req.Block.GetNumber(),
+	if ctx.Sender() != nil {
+		resp := &message.ReceiveBlockResp{
+			BlockNum: req.Block.GetNumber(),
+		}
+		if err != nil {
+			resp.ErrorNo = 1
+		} else {
+			resp.ErrorNo = 0
+		}
+		ctx.Sender().Request(resp, ctx.Self())
 	}
-	if err != nil {
-		resp.ErrorNo = 1
-	} else {
-		resp.ErrorNo = 0
-	}
-	NetActorPid.Tell(resp)
 
 	if err == nil {
 		req := &message.RemovePendingTrxsReq{Trxs: req.Block.Transactions}
