35d04fc28b2a7c81b9e9d086ff9db38438316892
huaapp <jiawang06@163.com>
2018-07-24 20:40:14 +0800

    Add dockerfile and script

---
+++ docker/botNode.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd /go/bin
+echo "Start conusl..."
+
+nohup consul agent -dev > consul.log 2>&1 &
+	sleep 3
+echo "Start micro..."
+nohup micro api > micro.log 2>&1 &
+
+cd /go/src/github.com/bottos-project/bottos/
+
+echo "Start bottosChain..."
+echo "It will take several sencond..."
+nohup ./bottos > bottos.log 2>&1 &
+tail -F bottos.log
+#tail -F core.log
