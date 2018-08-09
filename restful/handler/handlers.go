9aaf63fb22a5de80d957bf38eb2a94a828b2498f
huaapp <jiawang06@163.com>
2018-08-09 17:12:03 +0800

    Add getblock getaccount

--- restful/handler/handlers.go
+++ restful/handler/handlers.go
@@ -16,11 +16,19 @@ import (
 
 	bottosErr "github.com/bottos-project/bottos/common/errors"
 	"github.com/bottos-project/bottos/api"
+	"github.com/bottos-project/bottos/common"
+	"github.com/bottos-project/bottos/role"
 )
 
 //ApiService is actor service
 type ApiService struct {
 	env *env.ActorEnv
+}
+
+var roleIntf role.RoleInterface
+//SetChainActorPid set chain actor pid
+func SetRoleIntf(tpid role.RoleInterface) {
+	roleIntf = tpid
 }
 
 var chainActorPid *actor.PID
@@ -107,44 +115,49 @@ func GetInfo(w http.ResponseWriter, r *http.Request) {
 	resp.Errcode = 0
 	json.NewEncoder(w).Encode(resp)
 }
+
 //GetBlock query block
 func GetBlock(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome GetBlock !\n")
-}
+	//params := mux.Vars(r)
+	var msgReq *message.QueryBlockReq
+	_ = json.NewDecoder(r.Body).Decode(&msgReq)
 
-/*func (a *ApiService) GetBlock(ctx context.Context, req *api.GetBlockRequest, resp *api.GetBlockResponse) error {
-	msgReq := &message.QueryBlockReq{
-		BlockHash:   common.HexToHash(req.BlockHash),
-		BlockNumber: req.BlockNum,
-	}
 	res, err := chainActorPid.RequestFuture(msgReq, 500*time.Millisecond).Result()
+	var resp Todo
 	if err != nil {
 		resp.Errcode = uint32(bottosErr.ErrApiBlockNotFound)
 		resp.Msg = bottosErr.GetCodeString(bottosErr.ErrApiBlockNotFound)
-		return nil
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
 	}
 
 	response := res.(*message.QueryBlockResp)
 	if response.Block == nil {
 		resp.Errcode = uint32(bottosErr.ErrApiBlockNotFound)
 		resp.Msg = bottosErr.GetCodeString(bottosErr.ErrApiBlockNotFound)
-		return nil
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
 	}
 
-	resp.Result = &api.GetBlockResponse_Result{}
+	result := &api.GetBlockResponse_Result{}
 	hash := response.Block.Hash()
-	resp.Result.PrevBlockHash = response.Block.GetPrevBlockHash().ToHexString()
-	resp.Result.BlockNum = response.Block.GetNumber()
-	resp.Result.BlockHash = hash.ToHexString()
-	resp.Result.CursorBlockLabel = hash.Label()
-	resp.Result.BlockTime = response.Block.GetTimestamp()
-	resp.Result.TrxMerkleRoot = response.Block.ComputeMerkleRoot().ToHexString()
-	resp.Result.Delegate = string(response.Block.GetDelegate())
-	resp.Result.DelegateSign = response.Block.GetDelegateSign().ToHexString()
+	result.PrevBlockHash = response.Block.GetPrevBlockHash().ToHexString()
+	result.BlockNum = response.Block.GetNumber()
+	result.BlockHash = hash.ToHexString()
+	result.CursorBlockLabel = hash.Label()
+	result.BlockTime = response.Block.GetTimestamp()
+	result.TrxMerkleRoot = response.Block.ComputeMerkleRoot().ToHexString()
+	result.Delegate = string(response.Block.GetDelegate())
+	result.DelegateSign = response.Block.GetDelegateSign().ToHexString()
+	resp.Result = result
 
 	resp.Errcode = 0
-	return nil
-}*/
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		panic(err)
+	}
+}
 
 func SendTransaction(w http.ResponseWriter, r *http.Request) {
 
@@ -152,9 +165,51 @@ func SendTransaction(w http.ResponseWriter, r *http.Request) {
 func GetTransaction(w http.ResponseWriter, r *http.Request) {
 
 }
-
+//GetAccount query account info
 func GetAccount(w http.ResponseWriter, r *http.Request) {
+	var msgReq api.GetAccountRequest
+	_ = json.NewDecoder(r.Body).Decode(&msgReq)
+	name := msgReq.AccountName
 
+	account, err := roleIntf.GetAccount(name)
+	var resp Todo
+	if err != nil {
+		resp.Errcode = uint32(bottosErr.ErrApiAccountNotFound)
+		resp.Msg = bottosErr.GetCodeString(bottosErr.ErrApiAccountNotFound)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
+	}
+
+	balance, err := roleIntf.GetBalance(name)
+	if err != nil {
+		resp.Errcode = uint32(bottosErr.ErrApiAccountNotFound)
+		resp.Msg = bottosErr.GetCodeString(bottosErr.ErrApiAccountNotFound)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
+	}
+
+	stakedBalance, err := roleIntf.GetStakedBalance(name)
+	if err != nil {
+		resp.Errcode = uint32(bottosErr.ErrApiAccountNotFound)
+		resp.Msg = bottosErr.GetCodeString(bottosErr.ErrApiAccountNotFound)
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
+	}
+
+	result := &api.GetAccountResponse_Result{}
+	result.AccountName = name
+	result.Pubkey = common.BytesToHex(account.PublicKey)
+	result.Balance = balance.Balance
+	result.StakedBalance = stakedBalance.StakedBalance
+	resp.Result=result
+	resp.Errcode = 0
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		panic(err)
+	}
 }
 
 func GetKeyValue(w http.ResponseWriter, r *http.Request) {
