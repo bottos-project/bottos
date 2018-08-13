10cca9f44f98f1b3f7df8e5a482af2fdf2f49894
lyp830414 <lyp830414@163.com>
2018-08-13 01:20:45 -0700

    [Bottos] Add test case for get function

--- bcli/bcli_trx_test.go
+++ bcli/bcli_trx_test.go
@@ -24,3 +24,17 @@ func Test_PushTransaction(t *testing.T) {
 	cli.BcliPushTransaction(&pushtrx)
 }
 
+func Test_GetTransaction(t *testing.T) {
+	cli := NewCLI()
+	fmt.Println(cli)
+	CONFIG = &CLIConfig{}
+	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
+	CONFIG.ChainId  = "00000000000000000000000000000000"
+	
+	trxhash := "17b6167249e6b854e3dbcb7c5144f0763aa464ad245d9939ef96814c2fceb9d6"
+	
+	
+	//fmt.Printf("BcliPushTransaction %v!!!", pushtrx)
+	
+	cli.BcliGetTransaction(trxhash)
+}
