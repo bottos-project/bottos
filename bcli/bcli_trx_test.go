73fc99ca4ae9344c831b71f705ad3190310d62e8
lyp830414 <lyp830414@163.com>
2018-08-15 00:12:09 -0700

    [bottos] Support test cases

--- bcli/bcli_trx_test.go
+++ bcli/bcli_trx_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 func Test_PushTransaction(t *testing.T) {
 	cli := NewCLI()
@@ -24,6 +25,7 @@ func Test_GetTransaction(t *testing.T) {
 	CONFIG = &CLIConfig{}
 	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
 	CONFIG.ChainId  = "00000000000000000000000000000000"
+	CONFIG.ChainAddr = "127.0.0.1:8689"
 	
 	trxhash := "683a6343f56655f392887888407b48a90e83e3338e71e1cd802cafb93cdb19f4"
 	
@@ -38,3 +40,25 @@ func Test_DeployCode(t *testing.T) {
 	CONFIG.ChainAddr = "127.0.0.1:8689"
 	cli.deploycode("restful", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.wasm")
 }
+
+func Test_DeployAbi(t *testing.T) {
+	cli := NewCLI()
+	CONFIG = &CLIConfig{}
+	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
+	CONFIG.ChainId  = "00000000000000000000000000000000"
+	CONFIG.ChainAddr = "127.0.0.1:8689"
+	cli.deployabi("grpc", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.abi")
+}
+
+func Test_DeployCodeAndAbi(t *testing.T) {
+	cli := NewCLI()
+	CONFIG = &CLIConfig{}
+	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
+	CONFIG.ChainId  = "00000000000000000000000000000000"
+	CONFIG.ChainAddr = "127.0.0.1:8689"
+	cli.deploycode("restful", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.wasm")
+	time.Sleep(time.Duration(1) * time.Second)
+	cli.deployabi("grpc", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.abi")
+}
+
+
