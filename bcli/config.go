fa6ea9da2069f4bd1fa7fa15269832e271f2890b
lyp830414 <lyp830414@163.com>
2018-08-15 00:13:09 -0700

    [bottos] commit configs

--- bcli/config.go
+++ bcli/config.go
@@ -46,7 +46,7 @@ var CONFIG *CLIConfig
 type CLIConfig struct {
 	KeyPairs []KeyPair `json:"key_pairs"`
 	ChainId  string `json:"chain_id"`
-	ChainAddr string `json: "chain_addr"`
+	ChainAddr string `json:"chain_addr"`
 }
 
 //KeyPair key pair
@@ -70,7 +70,6 @@ func LoadConfig() error {
 		return e
 	}
 	CONFIG = &config
-
 	return nil
 }
 
