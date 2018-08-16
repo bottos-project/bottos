e117875982aebe2626a90dd57489da5f77637287
lyp830414 <lyp830414@163.com>
2018-08-16 00:31:21 -0700

    [bottos] Add test cases

--- bcli/bcli_trx_test.go
+++ bcli/bcli_trx_test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"testing"
-	"time"
+	//"time"
+	"io/ioutil"
 )
+
 func Test_PushTransaction(t *testing.T) {
 	cli := NewCLI()
 	CONFIG = &CLIConfig{}
@@ -16,7 +18,7 @@ func Test_PushTransaction(t *testing.T) {
         pushtrx.method   = "reg"
         pushtrx.ParamMap = map[string]interface{}{"nodeIP":"0a0a0a0a", "clusterIP":"0b0b0b0b", "uuid":"33", "capacity":"2GB"}
 
-	cli.BcliPushTransaction("restful", "http://127.0.0.1:8689/v1/transaction/send", &pushtrx)
+	cli.BcliPushTransaction("restful", "http://192.168.2.189:8689/v1/transaction/send", &pushtrx)
 
 }
 
@@ -25,11 +27,11 @@ func Test_GetTransaction(t *testing.T) {
 	CONFIG = &CLIConfig{}
 	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
 	CONFIG.ChainId  = "00000000000000000000000000000000"
-	CONFIG.ChainAddr = "127.0.0.1:8689"
+	CONFIG.ChainAddr = "192.168.2.189:8689"
 	
-	trxhash := "683a6343f56655f392887888407b48a90e83e3338e71e1cd802cafb93cdb19f4"
+	trxhash := "3711490b5cbd86d82f98d8d635ed80685460577f05eaf05a698d74b3f161d60b"
 	
-	cli.BcliGetTransaction("restful", "http://127.0.0.1:8689/v1/transaction/get", trxhash)
+	cli.BcliGetTransaction("restful", "http://192.168.2.189:8689/v1/transaction/get", trxhash)
 }
 
 func Test_DeployCode(t *testing.T) {
@@ -37,8 +39,8 @@ func Test_DeployCode(t *testing.T) {
 	CONFIG = &CLIConfig{}
 	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
 	CONFIG.ChainId  = "00000000000000000000000000000000"
-	CONFIG.ChainAddr = "127.0.0.1:8689"
-	cli.deploycode("restful", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.wasm")
+	CONFIG.ChainAddr = "192.168.2.189:8689"
+	cli.deploycode("restful", "http://192.168.2.189:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.wasm")
 }
 
 func Test_DeployAbi(t *testing.T) {
@@ -46,8 +48,8 @@ func Test_DeployAbi(t *testing.T) {
 	CONFIG = &CLIConfig{}
 	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
 	CONFIG.ChainId  = "00000000000000000000000000000000"
-	CONFIG.ChainAddr = "127.0.0.1:8689"
-	cli.deployabi("grpc", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.abi")
+	CONFIG.ChainAddr = "192.168.2.189:8689"
+	cli.deployabi("grpc", "http://192.168.2.189:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.abi")
 }
 
 func Test_DeployCodeAndAbi(t *testing.T) {
@@ -55,10 +57,20 @@ func Test_DeployCodeAndAbi(t *testing.T) {
 	CONFIG = &CLIConfig{}
 	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
 	CONFIG.ChainId  = "00000000000000000000000000000000"
-	CONFIG.ChainAddr = "127.0.0.1:8689"
-	cli.deploycode("restful", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.wasm")
+	CONFIG.ChainAddr = "192.168.2.189:8689"
+	cli.deploycode("restful", "http://192.168.2.189:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.wasm")
 	time.Sleep(time.Duration(1) * time.Second)
-	cli.deployabi("grpc", "http://127.0.0.1:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.abi")
+	cli.deployabi("grpc", "http://192.168.2.189:8689/v1/transaction/send", "nodeclustermng", "./nodeclustermng.abi")
 }
 
-
+func Test_GetContractCode(t *testing.T) {
+	cli := NewCLI()
+	CONFIG = &CLIConfig{}
+	CONFIG.KeyPairs = []KeyPair{{ PrivateKey: "b799ef616830cd7b8599ae7958fbee56d4c8168ffd5421a16025a398b8a4be45", PublicKey: "0454f1c2223d553aa6ee53ea1ccea8b7bf78b8ca99f3ff622a3bb3e62dedc712089033d6091d77296547bc071022ca2838c9e86dec29667cf740e5c9e654b6127f"},}
+	CONFIG.ChainId  = "00000000000000000000000000000000"
+	CONFIG.ChainAddr = "192.168.2.189:8689"
+	contractcode, abi := cli.BcliGetContractCode("nodeclustermng", "./nodeclustermng_saved.wasm", "./nodeclustermng_saved.abi")
+	writeFileToBinary(contractcode, "lypcontract.bin")
+        ioutil.WriteFile("lypabi.abi", []byte(abi), 0644)
+	
+}
