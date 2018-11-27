140e3f82062f5242f6436f5f2d850b8e7be298be
Song xuexian <s.xue.xian.c@gmail.com>
2018-11-27 20:09:44 +0800

    change check policy before load config file

--- config/check.go
+++ config/check.go
@@ -1,12 +1,10 @@
 package config
 
 import (
+	"encoding/hex"
 	"errors"
+	"net"
 	"regexp"
-)
-
-const (
-	ipReg = "(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)"
 )
 
 func SignKeyValidate(privateKey, publicKey string) (bool, error) {
@@ -15,16 +13,24 @@ func SignKeyValidate(privateKey, publicKey string) (bool, error) {
 	}
 	if len(privateKey) != 64 {
 		return false, errors.New("privateKey length error")
+	} else if _, err := isHex(privateKey); err != nil {
+		return false, errors.New("privateKey string error")
 	}
 
 	if len(publicKey) != 130 {
 		return false, errors.New("publicKey length error")
+	} else if _, err := isHex(publicKey); err != nil {
+		return false, errors.New("publicKey string error")
 	}
 	return true, nil
 }
 
+func isHex(key string) ([]byte, error) {
+	return hex.DecodeString(key)
+}
+
 func PortValidate(port int) (bool, error) {
-	if port < 1 && port > 65535 {
+	if port < 1 || port > 65535 {
 		return false, errors.New("port number invalidation")
 	}
 	return true, nil
@@ -34,7 +40,7 @@ func MongoUrlValidate(url string) (bool, error) {
 	if url == "" {
 		return true, nil
 	}
-	match, _ := regexp.MatchString("mongodb://[\\w+.+:\\w+.+@]+[\\w+.+:\\w+.+,]+[/\\w]*[?\\w+;=]+", url)
+	match, _ := regexp.MatchString("mongodb://[\\w+.+:\\w+.+@]+[\\d+.+:\\d+.+,]+[/\\w]*[?\\w+;=]+", url)
 
 	if false == match {
 		return false, errors.New("mongodb url error")
@@ -43,15 +49,16 @@ func MongoUrlValidate(url string) (bool, error) {
 	return true, nil
 }
 
-func IpValidate(ipAddr string) (bool, error) {
-	if ipAddr == "" {
+func IpValidate(ip string) (bool, error) {
+	if ip == "" {
 		return true, nil
 	}
-	match, _ := regexp.MatchString(ipReg, ipAddr)
-	if false == match {
-		return false, errors.New("ip address invalidation")
+	ipAddr := net.ParseIP(ip)
+	if ipAddr.To4() == nil && ipAddr.To16() == nil {
+		return false, errors.New("not an IPv4 or IPv6 address")
+	} else {
+		return true, nil
 	}
-	return true, nil
 }
 
 func IpValidateAll(ipAddrs []string) (bool, error) {
