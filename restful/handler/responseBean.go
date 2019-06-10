69e059e9b5da2502307136c742053d00f681d858
btosys <zhangshaokui@bottos.org>
2019-06-10 11:37:10 +0800

    add new account responseBean
    responseBean added

--- restful/handler/responseBean.go
+++ restful/handler/responseBean.go
@@ -1,9 +1,14 @@
 ﻿package handler
 
 type ResponseStruct struct {
-	Errcode uint32 `json:"errcode"`
-	Msg     string `json:"msg"`
-	Result    interface{} `json:"result"`
+	Errcode uint32      `json:"errcode"`
+	Msg     string      `json:"msg"`
+	Result  interface{} `json:"result"`
 }
 
 type ResponseStructs []ResponseStruct
+
+type NewAccount struct {
+	Account   string ` json:"account"`
+	PublicKey string `json:"public_key"`
+}
