ef458a0d8c82571a7ba14902006e2d6eb2351477
btosys <zhangshaokui@bottos.org>
2019-07-03 11:05:03 +0800

    add log route

---
+++ restful/log/routes.go
@@ -0,0 +1,44 @@
+﻿package logrestful
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	SET_CONFIGITEM = "/v1/log/setconfigitem"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	//wallet
+	Route{
+		"",
+		"POST",
+		SET_CONFIGITEM,
+		SetConfigItem,
+	},
+}
