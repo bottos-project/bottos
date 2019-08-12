e240d2913688fc2ea300bb33af33b4aa781c04d7
btosys <zhangshaokui@bottos.org>
2019-08-12 11:35:59 +0800

    Add log proto type

--- tool/pb-gen.bat
+++ tool/pb-gen.bat
@@ -2,7 +2,7 @@ rem generate common/types
 protoc\bin\protoc.exe -I ..\..\..\..\ --go_out=:..\..\..\..\  ..\..\..\..\github.com\bottos-project\bottos\common\types\transaction.proto ..\..\..\..\github.com\bottos-project\bottos\common\types\basic-transaction.proto ..\..\..\..\github.com\bottos-project\bottos\common\types\block.proto
 
 rem generate api
-protoc\bin\protoc.exe -I ..\..\..\..\ --go_out=:..\..\..\..\ --micro_out=:..\..\..\..\  ..\..\..\..\github.com\bottos-project\bottos\api\transaction.proto ..\..\..\..\github.com\bottos-project\bottos\api\basic-transaction.proto  ..\..\..\..\github.com\bottos-project\bottos\api\chain.proto
+protoc\bin\protoc.exe -I ..\..\..\..\ --go_out=:..\..\..\..\ --micro_out=:..\..\..\..\  ..\..\..\..\github.com\bottos-project\bottos\api\transaction.proto ..\..\..\..\github.com\bottos-project\bottos\api\basic-transaction.proto  ..\..\..\..\github.com\bottos-project\bottos\api\chain.proto   ..\..\..\..\github.com\bottos-project\bottos\api\log.proto
 
 rem generate tool/example
 protoc\bin\protoc.exe -I ..\..\..\..\ --go_out=:..\..\..\..\ --micro_out=:..\..\..\..\   ..\..\..\..\github.com\bottos-project\bottos\tool\example\example.proto
