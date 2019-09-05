8c4dc72a9e955d64d04f8ae7102be8e2797be1f6
btosys <zhangshaokui@bottos.org>
2019-09-05 14:51:40 +0800

    Fixed blanks

--- cmd/flags.go
+++ cmd/flags.go
@@ -26,32 +26,32 @@ package cmd
 
 import (
 	"gopkg.in/urfave/cli.v1"
-)
+	)
 
 var (
 	ConfigFileFlag = cli.StringFlag{
-		Name: "config",
+		Name:  "config",
 		Usage: "Config file path",
 	}
 	GenesisFileFlag = cli.StringFlag{
-		Name: "genesis",
+		Name:  "genesis",
 		Usage: "Genesis config file path",
 	}
 	DataDirFlag = cli.StringFlag{
-		Name: "datadir",
+		Name:  "datadir",
 		Usage: "Data directory for the databases",
 	}
 	DisableRESTFlag = cli.BoolFlag{
-		Name: "disable-api",
+		Name:  "disable-api",
 		Usage: "Disable RESTful API server",
 	}
 	RESTPortFlag = cli.IntFlag{
-		Name: "restport",
+		Name:  "restport",
 		Usage: "RESTful API server listening port",
 		Value: 8689,
 	}
 	RESTServerAddrFlag = cli.StringFlag{
-		Name: "rest-servaddr",
+		Name:  "rest-servaddr",
 		Usage: "RESTful API server address",
 	}
 	RestMaxLimit = cli.IntFlag{
@@ -66,72 +66,72 @@ var (
 	}
 
 	EnableRPCFlag = cli.BoolFlag{
-		Name: "enable-rpc",
+		Name:  "enable-rpc",
 		Usage: "Enable RPC server",
 	}
 	RPCPortFlag = cli.IntFlag{
-		Name: "rpcport",
+		Name:  "rpcport",
 		Usage: "RPC server listening port",
 		Value: 8690,
 	}
 	P2PPortFlag = cli.IntFlag{
-		Name: "p2pport",
+		Name:  "p2pport",
 		Usage: "P2P network listening port",
 		Value: 9868,
 	}
 	P2PServerAddrFlag = cli.StringFlag{
-		Name: "p2p-servaddr",
+		Name:  "p2p-servaddr",
 		Usage: "P2P network server address",
 	}
 	PeerListFlag = cli.StringFlag{
-		Name: "peerlist",
+		Name:  "peerlist",
 		Usage: "Comma separated list of network peers (192.168.1.2:9868,192.168.1.3:9868,192.168.1.4:9868)",
 	}
 	DelegateSignkeyFlag = cli.StringFlag{
-		Name: "delegate-signkey",
+		Name:  "delegate-signkey",
 		Usage: "Sign key for delegate ('key:<public key>,<private key>' or 'wallet:<wallet url>')",
 	}
 	DelegateFlag = cli.StringFlag{
-		Name: "delegate",
+		Name:  "delegate",
 		Usage: "Producer account name",
 	}
 	DelegatePrateFlag = cli.IntFlag{
-		Name: "delegate-prate",
+		Name:  "delegate-prate",
 		Usage: "Config delegate participate threshold",
 		Value: 33,
 	}
 	EnableMongoDBFlag = cli.BoolFlag{
-		Name: "enable-mongodb",
+		Name:  "enable-mongodb",
 		Usage: "Enable mongodb plugin",
 	}
 	MongoDBFlag = cli.StringFlag{
-		Name: "mongodb",
+		Name:  "mongodb",
 		Usage: "MongoDB connection config",
 	}
 	LogConfigFlag = cli.StringFlag{
-		Name: "logconfig",
+		Name:  "logconfig",
 		Usage: "Log config file path",
 	}
 	WalletDirFlag = cli.StringFlag{
-		Name: "walletdir",
+		Name:  "walletdir",
 		Usage: "wallet directory",
 	}
 	EnableWalletFlag = cli.BoolFlag{
-		Name: "enable-wallet",
+		Name:  "enable-wallet",
 		Usage: "enable wallet",
 	}
 	WalletRESTPortFlag = cli.IntFlag{
-		Name: "wallet-rest-port",
+		Name:  "wallet-rest-port",
 		Usage: "Wallet RESTful API server listening port",
 		Value: 6869,
 	}
 	WalletRESTServerAddrFlag = cli.StringFlag{
-		Name: "wallet-rest-servaddr",
+		Name:  "wallet-rest-servaddr",
 		Usage: "Wallet RESTful API server address",
 		Value: "localhost",
 	}
 	DebugFlag = cli.BoolFlag{
-		Name: "debug",
+		Name:  "debug",
 		Usage: "Enable debug mode",
 	}
 	LogMinLevelFlag = cli.StringFlag{
