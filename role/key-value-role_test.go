f8b179a03e161e9791c5202c5ef06f07bb5e8075
btosys <zhangshaokui@bottos.org>
2019-12-05 19:05:24 +0800

    Create key-value-role_test.go
    add new file

---
+++ role/key-value-role_test.go
@@ -0,0 +1,40 @@
+// Copyright 2017~2022 The Bottos Authors
+// This file is part of the Bottos Chain library.
+// Created by Rocket Core Team of Bottos.
+
+//This program is free software: you can distribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+// along with bottos.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * file description: code database test
+ * @Author: May Luo
+ * @Date:   2017-12-04
+ * @Last Modified by:
+ * @Last Modified time:
+ */
+package role
+
+import (
+	//	"encoding/json"
+	"testing"
+	"fmt"
+	log "github.com/cihub/seelog"
+
+	"github.com/bottos-project/bottos/db"
+)
+
+func Test_SetBinValue(t *testing.T) {
+
+	fmt.Println("success")
+
+}
