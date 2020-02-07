75ec6bffc5f8f35208b69d5a88459d1acda92383
bottoszw <zhangwei@bottos.org>
2020-02-07 17:10:06 +0800

    modify account data for state db

--- role/account-role_test.go
+++ role/account-role_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017~2022 The Bottos Authors
+﻿// Copyright 2017~2022 The Bottos Authors
 // This file is part of the Bottos Chain library.
 // Created by Rocket Core Team of Bottos.
 
@@ -75,6 +75,47 @@ func TestAccount_writedb(t *testing.T) {
 	}
 
 	if value.AccountName != value1.AccountName {
+		t.Fatal("Account Name error")
+	}
+	log.Info(value)
+
+
+
+	value3 := &Contract{
+		ContractName:  "account1",
+		
+		VMType:       0,
+		VMVersion:    1,
+		CodeVersion:  common.StringToHash("26427d49aca564c5774724de0e0b2eb1a7c4f303e73ce7dcef3b52e1ab95cc4b"),
+		ContractCode: []byte{},
+		ContractAbi:  []byte{},
+	}
+	value4 := &Contract{
+		ContractName:  "account2",
+		//PublicKey:    []byte("7QBxKhpppiy7q4AcNYKRY2ofb3mR5RP8ssMAX65VEWjpAgaAnF"),
+		VMType:       0,
+		VMVersion:    1,
+		CodeVersion:  common.StringToHash("26427d49aca564c5774724de0e0b2eb1a7c4f303e73ce7dcef3b52e1ab95cc4b"),
+		
+		ContractCode: []byte{},
+		ContractAbi:  []byte{},
+	}
+	err = SetContractRole(ins, value3.ContractName, value3)
+	if err != nil {
+		t.Fatal("SetContractRole", err)
+	}
+	err = SetContractRole(ins, value4.ContractName, value4)
+	if err != nil {
+		t.Fatal("SetContractRole", err)
+	}
+
+	var valueCtx *Contract
+	valueCtx, err = GetContractRole(ins, value3.ContractName)
+	if err != nil {
+		t.Fatal("GetContractRole", err)
+	}
+
+	if valueCtx.ContractName != value3.ContractName {
 		t.Fatal("Account Name error")
 	}
 	log.Info(value)
