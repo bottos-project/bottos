4d212497485bcf20db1e3d85a52340a3c9020fb0
bottoszw <zhangwei@bottos.org>
2020-03-20 13:38:30 +0800

    add consensus version

--- config/default-value.go
+++ config/default-value.go
@@ -107,6 +107,7 @@ const DEFAUL_MAX_NOTICE_CONTRACT_NUM uint32 = 5
 
 const PRIMARY_TRX_SESSION string = "primary"
 const SUB_TRX_SESSION string = "subsession"
+const CONSENSUS_VERSION string = "1.0"
 
 const TRX_IN uint16 = 0
 const TRX_IN_TTL uint16 = 1
